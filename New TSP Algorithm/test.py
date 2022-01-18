b7119f201fdcf5f44641f5bb5bd28d20f7146bdc
Shubhajit Roy <81477286+shubhajitroy123@users.noreply.github.com>
2022-01-18 12:09:31 +0530

    Add files via upload

---
+++ New TSP Algorithm/test.py
@@ -0,0 +1,7 @@
+import spacy
+from spacy import displacy
+
+nlp = spacy.load('en_core_web_sm')
+about_interest_text = ('He is interested in learning Natural Language Processing.')
+about_interest_doc = nlp(about_interest_text)
+displacy.render(about_interest_doc, style='dep')
